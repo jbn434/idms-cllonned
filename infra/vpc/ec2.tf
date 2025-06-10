5bf8620310925f4e486bacbeb8c38861e452e70c
sharon-ndz <ubuntu@ip-172-31-90-212.ec2.internal>
2025-06-10 21:12:17 +0000

    final

--- infra/vpc/ec2.tf
+++ infra/vpc/ec2.tf
@@ -1,9 +1,13 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-084568db4383264d4" # Replace with correct AMI for your region
+  ami           = var.ami_id
   instance_type = var.instance_type
 
   subnet_id              = module.vpc.public_subnets_ids[0]
-  #vpc_security_group_ids = [module.vpc.default_security_group_id]
   vpc_security_group_ids = [aws_security_group.open_ssh_and_app.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_ssm_profile.name
   key_name               = "githubaction"
@@ -18,3 +22,4 @@ resource "aws_instance" "web" {
     Name = "Backend API IDLMS"
   }
 }
+
